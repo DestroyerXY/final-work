7974b175e965a4a7a8d7686763863bd757e85b6a
«Ramil <Ramil1995@bk.ru>
2023-01-30 00:11:49 +0300

    CodeBody

--- Program.cs
+++ Program.cs
@@ -22,3 +22,20 @@ void NewArray (string[] arrayOfString, string[] arrayOfString1)
 Console.WriteLine("Итоговый массив: ");
 Console.WriteLine($"[{string.Join(", ", arrayOfString1)}]");
 }
+
+Console.Clear();
+Console.WriteLine("Введите данные через пробел: ");
+string[] arrayOfString = Console.ReadLine().Split(" ");
+Console.WriteLine("Первоначальный массив: ");
+Console.WriteLine($"[{string.Join(", ", arrayOfString)}]");
+int count = 0;
+for (int i = 0; i < arrayOfString.Length; i++)
+    {
+        int b = arrayOfString[i].Length;
+        if (b <= 3)
+        {
+            count ++;
+        }
+    }
+string[] arrayOfString1 = new string[count];
+NewArray (arrayOfString, arrayOfString1);
