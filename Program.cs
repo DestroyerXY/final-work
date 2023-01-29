dae5bc3b67628d0129bc8b3ccf2ce78ca60b92f9
«Ramil <Ramil1995@bk.ru>
2023-01-30 00:10:50 +0300

    Void

--- Program.cs
+++ Program.cs
@@ -6,3 +6,19 @@
 // Шаг 4: создаем новый массив arrayOfString1, размер которого равен переменной count
 // Шаг 5: создаем метод, который будет заполнять новый массив arrayOfString1 только подходящими нам элементами (длина строки меньше либо равна 3 символам)
 // Шаг 6: Выводим массив arrayOfString1 на экран
+
+void NewArray (string[] arrayOfString, string[] arrayOfString1)
+{
+    int j = 0;
+    for (int i = 0; i < arrayOfString.Length; i++)
+        {
+            int b = arrayOfString[i].Length;
+            if (b <= 3)
+            {
+                arrayOfString1[j] = arrayOfString[i];
+                j++;
+            }
+        }
+Console.WriteLine("Итоговый массив: ");
+Console.WriteLine($"[{string.Join(", ", arrayOfString1)}]");
+}
